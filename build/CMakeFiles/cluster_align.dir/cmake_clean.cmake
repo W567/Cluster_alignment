363cbabd0bd4ee76013a38aa4466444c4e2876bf
Wuliqi <wlqwa567@gmail.com>
2018-05-13 16:48:46 +0900

    add libForPCL

---
+++ build/CMakeFiles/cluster_align.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cluster_align.dir/src/cluster_alignment.cpp.o"
+  "CMakeFiles/cluster_align.dir/src/libForPCL/src/cook_geometry.cpp.o"
+  "CMakeFiles/cluster_align.dir/src/libForPCL/src/alignment.cpp.o"
+  "CMakeFiles/cluster_align.dir/src/libForPCL/src/cook_io.cpp.o"
+  "CMakeFiles/cluster_align.dir/src/libForPCL/src/cook_basis.cpp.o"
+  "CMakeFiles/cluster_align.dir/src/libForPCL/src/cook_seg.cpp.o"
+  "../cluster_align.pdb"
+  "../cluster_align"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cluster_align.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
